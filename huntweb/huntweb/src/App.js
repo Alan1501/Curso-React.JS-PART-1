ec840bf231e7e92a236f480291d6b1c6d3d1c6c8
Alan <alan.andersonbr@gmail.com>
2020-06-11 19:38:01 -0300

    arquivo Src do React

---
+++ huntweb/huntweb/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import ".styles.css";
+
+import Header from './components/header';
+
+class App extends Component {
+ render () {
+  return (
+    <div className="App">
+     <header />
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ huntweb/huntweb/src/components/header/index.js
@@ -0,0 +1,8 @@
+import react from 'react'
+
+import './styles.css'; 
+
+const Header = () => <header id ="main-header">JSHunt</header> 
+
+export default Header;
+
